839dccca75b550e962834c8cf0556d043797a77f
zach <zachbogard@gmail.com>
2019-06-13 16:10:46 -0700

    ep14

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Ninjas from './Ninjas'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 30, belt: 'black', id: 2 },
+      { name: 'Crystal', age: 30, belt: 'pink', id: 3 }
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app!</h1>
+        <p>Welcome :)</p>
+        <Ninjas ninjas={this.state.ninjas} />
+      </div>
+    )
+  }
+
 }
 
 export default App;
