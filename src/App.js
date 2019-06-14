665e6fb2864abadd2cbfb36b2d4e48366e45ac50
zach <zachbogard@gmail.com>
2019-06-13 18:34:06 -0700

    ep16

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ class App extends Component {
   state = {
     ninjas: [
       { name: 'Ryu', age: 30, belt: 'black', id: 1 },
-      { name: 'Yoshi', age: 30, belt: 'black', id: 2 },
-      { name: 'Crystal', age: 30, belt: 'pink', id: 3 }
+      { name: 'Yoshi', age: 25, belt: 'black', id: 2 },
+      { name: 'Crystal', age: 17, belt: 'pink', id: 3 }
     ]
   }
   render() {
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,20 +1,20 @@
 import React from 'react';
-                // Destructuring
-const Ninjas = ({ninjas}) => {
-   
-    // Take an old array and push it into a new array
-    const ninjaList = ninjas.map(ninja => {
-        return (
-            <div className="ninja" key={ninja.id}>
-                <div>Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div>Belt: {ninja.belt}</div>
-            </div>
-        )
-    })
+// Destructuring
+const Ninjas = ({ ninjas }) => {
+
     return (
         <div className="ninja-list">
-            {ninjaList}
+            {
+                ninjas.map(ninja => {
+                    return ninja.age > 20 ? (
+                        <div className="ninja" key={ninja.id}>
+                            <div>Name: {ninja.name}</div>
+                            <div>Age: {ninja.age}</div>
+                            <div>Belt: {ninja.belt}</div>
+                        </div>
+                    ) : null
+                })
+            }
         </div>
     )
 }
