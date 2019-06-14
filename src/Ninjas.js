60ea4021003c8fa00fc58d3f24b806da4e7bfa75
zach <zachbogard@gmail.com>
2019-06-13 19:03:44 -0700

    ep17

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,20 +1,20 @@
 import React from 'react';
 // Destructuring
 const Ninjas = ({ ninjas }) => {
-
+    const ninjaList = ninjas.map(ninja => {
+        if (ninja.age > 20) {
+            return (
+                <div className="ninja" key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                </div>
+            )
+        }
+    })
     return (
         <div className="ninja-list">
-            {
-                ninjas.map(ninja => {
-                    return ninja.age > 20 ? (
-                        <div className="ninja" key={ninja.id}>
-                            <div>Name: {ninja.name}</div>
-                            <div>Age: {ninja.age}</div>
-                            <div>Belt: {ninja.belt}</div>
-                        </div>
-                    ) : null
-                })
-            }
+            {ninjaList}
         </div>
     )
 }
