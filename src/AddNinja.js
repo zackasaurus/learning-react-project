8d84bb40137e66dcd508e948eea28b134604e9b3
zach <zachbogard@gmail.com>
2019-06-13 20:14:10 -0700

    ep18

--- src/AddNinja.js
+++ src/AddNinja.js
@@ -14,7 +14,8 @@ class AddNinja extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault()
-        console.log(this.state)
+        this.props.addNinja(this.state)
+        
     }
     render() {
         return (
--- src/App.js
+++ src/App.js
@@ -9,13 +9,22 @@ class App extends Component {
       { name: 'Crystal', age: 17, belt: 'pink', id: 3 }
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    // spread operator
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas
+    })
+    // console.log(ninja)
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome :)</p>
         <Ninjas ninjas={this.state.ninjas} />
-        <AddNinja />
+        <AddNinja addNinja={this.addNinja} />
       </div>
     )
   }
